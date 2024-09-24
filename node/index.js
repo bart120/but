5be86698b8ea397537c3a5d968c7c62ad9a8e324
Vincent Leclerc <vleclerc@inow.fr>
2024-09-24 12:11:41 +0200

    change python

--- node/index.js
+++ node/index.js
@@ -1,3 +1,12 @@
 const redis = require('redis');
 const mysql = require('mysql2/promise');
 const config = require('./config');
+
+//connexion à redis
+const redisClient = redis.createClient({
+    host: 'localhost',
+    port: 6379
+});
+
+//connexion à mysql
+const db = mysql.createPool(config.mysql);
