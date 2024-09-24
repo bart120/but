cd734bca22729463f67ea048a76134d6eca57a76
Vincent Leclerc <vleclerc@inow.fr>
2024-09-24 14:55:56 +0200

    en python

--- py/main.py
+++ py/main.py
@@ -2,6 +2,8 @@ import redis
 import mysql.connector
 import json
 from config import config
+from decimal import Decimal
+
 
 #connexion à redis
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
@@ -10,14 +12,30 @@ redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 db = mysql.connector.connect(**config)
 cursor = db.cursor(dictionary=True)
 
+def custom_serializer(obj):
+    if isinstance(obj, Decimal):
+        return float(obj)
+
 def get_produit_from_db(produit_id):
     cursor.execute("SELECT * FROM produits WHERE id=%s", (produit_id,))
     produit = cursor.fetchone()
     return produit
 
+def get_produit(produit_id):
+    cache_key = f'produit:{produit_id}'
+    produit_cache = redis_client.get(cache_key)
+
+    if produit_cache:
+        return json.loads(produit_cache)
+    else:
+        produit = get_produit_from_db(produit_id)
+        if produit:
+            redis_client.setex(cache_key, 600, json.dumps(produit, default=custom_serializer))
+        return produit
+
 #execution
 if __name__ == '__main__':
-    produit = get_produit_from_db(1)
+    produit = get_produit(1)
     print(produit)
     cursor.close()
     db.close()
